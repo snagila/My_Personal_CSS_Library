be9669c39998e5e04956c0e0ead85140b4a63a0a
Sachin Nagila <sachinnagila2053@gmail.com>
2024-06-18 00:00:55 +1000

    scrollable table single and grouping

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./Components/header/Header";
 import "./Components/header/header.css";
+import ScrollTable from "./Components/scrollableTables/ScrollTable";
 
 function App() {
   return (
     <>
       <Header />
+      <ScrollTable />
     </>
   );
 }
